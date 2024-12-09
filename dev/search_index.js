04b7c0a0291626c2c5b942522209080d2df2c75f
Documenter.jl <documenter@juliadocs.github.io>
2024-12-09 10:11:54 +0000

    build based on 1bb97ec

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = TrixiWave","category":"page"},{"location":"#TrixiWave","page":"Home","title":"TrixiWave","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for TrixiWave.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [TrixiWave]","category":"page"}]
+}
